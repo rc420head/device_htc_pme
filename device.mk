e0aeac3743ed82360b59aa73d86399d4ee1c7ba2
Rashed Abdel-Tawab <rashed@linux.com>
2017-08-22 14:30:20 -0700

    TEMP: disable lineage overlays and LGDoze
    Change-Id: I04751038e73c385bd1cddd43ec870c1e308b96d4

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/htc/pme/pme-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+#DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
