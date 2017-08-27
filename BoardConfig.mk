519cab75ae2a62a4b00ffaf06aafd54af432d5dd
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-08-27 21:57:53 +0200

    msm8996: selinux: don't use device specific policies
    Change-Id: Ib5b94300ded6ab8a474ad4e001641fd6f7ace325

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 # include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Wifi
 TARGET_WLAN_VARIANT := caf
